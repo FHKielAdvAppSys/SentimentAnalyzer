a2870aa0f65da1e2010a08733bd01f881a7581bd
adamritadas <adamritadas@gmail.com>
2017-04-27 03:46:32 +0530

    Bing Search API logic Implemented

--- SentimentAnalyzer/Service/BingService.cs
+++ SentimentAnalyzer/Service/BingService.cs
@@ -5,6 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 using SentimentAnalyzer.Core;
 using SentimentAnalyzer.Model;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Web;
+using System.Net;
+using System.Runtime.Serialization.Json;
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
 
 namespace SentimentAnalyzer.Service
 {
@@ -27,7 +34,38 @@ namespace SentimentAnalyzer.Service
                 new SearchResult { ID = 2, Title = "title2", Description = "scription2", Url = "www.url2.com" },
                 new SearchResult { ID = 3, Title = "title3", Description = "scription3", Url = "www.url3.com" }
             };
+
+           // MakeRequest(); // Uncomment this to Call Bing Search API
+
             return mockListResult;
+
+
         }
+
+        static async void MakeRequest()
+        {
+
+            var client = new HttpClient();
+            var queryString = HttpUtility.ParseQueryString(string.Empty);
+
+            // Request headers
+            client.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", "e58407b94ffc4d4d99c6a6b6eb711051");
+
+            // Request parameters
+            queryString["q"] = "bill gates";
+            queryString["count"] = "10";
+            queryString["offset"] = "0";
+            queryString["mkt"] = "en-us";
+            queryString["safesearch"] = "Moderate";
+            queryString["JsonType"] = "raw";  
+            var uri = "https://api.cognitive.microsoft.com/bing/v5.0/search?" + queryString;
+
+            var response = await client.GetStringAsync(uri);
+
+            var searchResult = JsonConvert.DeserializeObject(response);
+
+
+        }
+
     }
 }
